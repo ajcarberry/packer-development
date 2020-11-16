7a02dcce4775440dfd744ccca54786de2812c387
Alex <alex.carberry@epicgames.com>
2020-11-15 23:28:29 -0500

    fix code-server installer

--- scripts/code-server.sh
+++ scripts/code-server.sh
@@ -1,15 +1,26 @@
 #!/bin/bash -eux
 
+max_retry=20
+retry=0
+
 # Install code-server
 curl -fsSL https://code-server.dev/install.sh | sh
 
 # Enable and start code-server
-sudo systemctl enable --now code-server@$USER
+systemctl enable --now code-server@$USER
+systemctl start code-server@$USER
 
 # Disable auth and bind to port 8080
+while [ ${retry} -lt ${max_retry} ]; do
+   if [ -f ~/.config/code-server/config.yaml ]; then
+      break
+   else
+      (( retry = retry + 1 ))
+      sleep 1
+   fi
+done
 sed -i.bak 's/auth: password/auth: none/' ~/.config/code-server/config.yaml
 sed -i.bak 's/bind-addr: 127\.0\.0\.1\:8080/bind-addr: 0\.0\.0\.0\:8080/' ~/.config/code-server/config.yaml
 
 # Restart code-server
-sudo systemctl stop code-server@$USER
-sudo systemctl start code-server@$USER
+systemctl restart code-server@$USER
